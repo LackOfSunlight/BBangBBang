c168643901cae06d652f016aa0ddc2c0e52577d4
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-09-08 13:21:03 +0900

    ✨ feat:"방 생성 구현"

--- game.server/src/handlers/request/create.room.request.handler.ts
+++ game.server/src/handlers/request/create.room.request.handler.ts
@@ -1,26 +1,97 @@
-import { Socket } from "net";
-import { C2SCreateRoomRequest } from "../../generated/packet/room_actions.js";
 import { GamePacket } from "../../generated/gamePacket.js";
 import { getGamePacketType } from "../../utils/type.converter.js";
-import { gamePackTypeSelect } from "../../enums/gamePacketType.js";
+import {
+  GamePacketType,
+  gamePackTypeSelect,
+} from "../../enums/gamePacketType.js";
 import { RoomData } from "../../generated/common/types.js";
-import { RoomStateType } from "../../generated/common/enums.js";
+import {
+  GlobalFailCode,
+  RoomStateType as StateType,
+} from "../../generated/common/enums.js";
 import { GameSocket } from "../../type/game.socket.js";
+import createRoomResponseHandler from "../response/create.room.response.handler.js";
+import { Room } from "../../models/room.model.js";
+import { prisma } from "../../utils/db.js";
+import { RoomStateType } from "../../generated/prisma/index.js";
+import { User } from "../../models/user.model.js";
 
-const createRoomRequestHandler = (socket:GameSocket, gamePacket:GamePacket) =>{
-    const payload = getGamePacketType(gamePacket, gamePackTypeSelect.createRoomRequest);
-    if(payload){
+const createRoomRequestHandler = async (
+  socket: GameSocket,
+  gamePacket: GamePacket
+) => {
+  const payload = getGamePacketType(
+    gamePacket,
+    gamePackTypeSelect.createRoomRequest
+  );
+  if (!payload || !socket.userId)
+    return createRoomResponseHandler(
+      socket,
+      setCreateResponse(false, GlobalFailCode.CREATE_ROOM_FAILED)
+    );
 
-        const roomData:RoomData ={
-            id: 0,
-            ownerId: "누가 만듬?",
-            name: payload.createRoomRequest.name,
-            maxUserNum: payload.createRoomRequest.maxUserNum,
-            state: RoomStateType.WAIT,
-            users: [],
-        }
-    }
+  const req = payload.createRoomRequest;
 
-}
+  if (!req.name) {
+    return createRoomResponseHandler(
+      socket,
+      setCreateResponse(false, GlobalFailCode.CREATE_ROOM_FAILED)
+    );
+  }
 
-export default createRoomRequestHandler
+  const roomDB = await prisma.room.create({
+    data: {
+      ownerId: Number(socket.userId),
+      name: req.name,
+      maxUserNum: req.maxUserNum,
+      state: RoomStateType.WAIT,
+    },
+  });
+
+  const userInfo = await prisma.user.findUnique({
+    where: { id: Number(socket.userId) },
+  });
+
+  if (!userInfo)
+    return createRoomResponseHandler(
+      socket,
+      setCreateResponse(false, GlobalFailCode.CREATE_ROOM_FAILED)
+    );
+
+  const user: User = new User(userInfo.id.toString(), userInfo.nickname);
+
+  const room: Room = new Room(
+    roomDB.id,
+    socket.userId,
+    req.name,
+    req.maxUserNum,
+    StateType.WAIT,
+    [user]
+  );
+
+  createRoomResponseHandler(
+    socket,
+    setCreateResponse(true, GlobalFailCode.NONE_FAILCODE, room)
+  );
+};
+
+const setCreateResponse = (
+  success: boolean,
+  failCode: GlobalFailCode,
+  room?: RoomData
+): GamePacket => {
+  const newGamePacket: GamePacket = {
+    payload: {
+      oneofKind: GamePacketType.createRoomResponse,
+      createRoomResponse: {
+        success,
+        room,
+        failCode,
+      },
+    },
+  };
+
+  return newGamePacket;
+};
+
+export default createRoomRequestHandler;
--- game.server/src/handlers/response/create.room.response.handler.ts
+++ game.server/src/handlers/response/create.room.response.handler.ts
@@ -1,9 +1,21 @@
 import { GameSocket } from "../../type/game.socket.js";
-import { S2CCreateRoomResponse } from "../../generated/packet/room_actions.js";
 import { GamePacket } from "../../generated/gamePacket.js";
+import { saveRoom } from "../../utils/redis.util.js";
+import { getGamePacketType } from "../../utils/type.converter.js";
+import { GamePacketType, gamePackTypeSelect } from "../../enums/gamePacketType.js";
+import { sendData } from "../../utils/send.data.js";
 
-const createRoomResponseHandler = (socket:GameSocket, gamePacket:GamePacket) =>{
+const createRoomResponseHandler = async (socket:GameSocket, gamePacket:GamePacket) =>{
 
+    const payload = getGamePacketType(gamePacket, gamePackTypeSelect.createRoomResponse);
+    if(!payload || !socket.userId) return;
+    const res = payload.createRoomResponse;
+
+    if(!res.room) return;
+
+    await saveRoom(res.room);
+
+    sendData(socket, gamePacket, GamePacketType.createRoomResponse);
 }
 
 
