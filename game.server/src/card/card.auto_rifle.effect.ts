8c5a5b40f1b51e335618cf22bd9731c1b76e3b8f
kimdandy <d22psource@gmail.com>
2025-09-11 14:05:41 +0900

    ✨ feat:'저장 실패  에러문 추가'

--- game.server/src/card/card.auto_rifle.effect.ts
+++ game.server/src/card/card.auto_rifle.effect.ts
@@ -21,7 +21,11 @@ const cardAutoRifleEffect = async (roomId:number, userId:string) =>{
     //// 손에 Bbang 카드가 추가될 경우
 
     // 수정 정보 갱신
+    try {
     await updateCharacterFromRoom(roomId, userId, user.character);
+    } catch (error) {
+        console.error('[저장 실패]:', error);
+    }
 }
 
 
