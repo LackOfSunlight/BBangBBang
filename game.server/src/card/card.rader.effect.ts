f6e3f9270ee3aff399f68d5da14ccd7e618d4612
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-09-11 11:42:43 +0900

    ♻️ refactor:"레이더 중복 장착 방지"

--- game.server/src/card/card.rader.effect.ts
+++ game.server/src/card/card.rader.effect.ts
@@ -1,24 +1,24 @@
 // cardType = 18
-import { getUserFromRoom, updateCharacterFromRoom } from "../utils/redis.util.js";
-import { CardType } from "../generated/common/enums.js";
+import { getUserFromRoom, updateCharacterFromRoom } from '../utils/redis.util.js';
+import { CardType } from '../generated/common/enums.js';
 
-const cardRaderEffect = async (roomId:number, userId:string) =>{
-    const user = await getUserFromRoom(roomId, userId);
+const cardRaderEffect = async (roomId: number, userId: string) => {
+	const user = await getUserFromRoom(roomId, userId);
 
-    // 유효성 검증
-    if (!user || !user.character) return; 
+	// 유효성 검증
+	if (!user || !user.character) return;
 
-    user.character.equips.push(CardType.RADAR);
-    
-    // Redis에 업데이트된 캐릭터 정보 저장
-    try {
-            await updateCharacterFromRoom(roomId, user.id, user.character);
-        } catch (error) {
-            console.error(`[스나이퍼] Redis 업데이트 실패:`, error);
-            // 에러가 발생해도 함수는 정상적으로 완료됨
-        }
+	if (!user.character.equips.includes(CardType.RADAR)) {
+		user.character.equips.push(CardType.RADAR);
 
-}
-
+		// Redis에 업데이트된 캐릭터 정보 저장
+		try {
+			await updateCharacterFromRoom(roomId, user.id, user.character);
+		} catch (error) {
+			console.error(`[레이더] Redis 업데이트 실패:`, error);
+			// 에러가 발생해도 함수는 정상적으로 완료됨
+		}
+	}
+};
 
 export default cardRaderEffect;
