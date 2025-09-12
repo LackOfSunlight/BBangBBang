41d275395fd4ec5357e6ae74ded81b1c6ba68f68
gguti <ggutips94@gmail.com>
2025-09-12 19:53:32 +0900

    ✨ feat 애니매이션 노티피케이션 기능 추가
    애니메이션을 노티피케이션 하는 기능 추가하고 자동쉴드가 발동 될 때 보내지도록 기능 추가

--- game.server/src/handlers/notification/animation.notification.handler.ts
+++ game.server/src/handlers/notification/animation.notification.handler.ts
@@ -1,7 +1,28 @@
 import { GameSocket } from '../../type/game.socket.js';
 import { S2CAnimationNotification } from '../../generated/packet/notifications.js';
 import { GamePacket } from '../../generated/gamePacket.js';
-import { GamePacketType } from '../../enums/gamePacketType';
+import { GamePacketType } from '../../enums/gamePacketType.js';
+import { broadcastDataToRoom } from '../../utils/notification.util.js';
+import { AnimationType } from '../../generated/common/enums.js';
+import { UserData } from '../../generated/common/types.js';
+
+export const sendAnimationNotification = (
+	users: UserData[],
+	userId: string,
+	animationType: AnimationType,
+) => {
+	const newGamePacket: GamePacket = {
+		payload: {
+			oneofKind: GamePacketType.s2cAnimationNotification,
+			s2cAnimationNotification: {
+				userId: userId,
+				animationType: animationType,
+			},
+		},
+	};
+
+	broadcastDataToRoom(users, newGamePacket, GamePacketType.s2cAnimationNotification);
+};
 
 const animationNotificationHandler = (socket: GameSocket, gamePacket: GamePacket) => {};
 
