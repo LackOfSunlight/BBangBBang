682153dcae9b375f9b7e701ae778e1a192e443fb
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-09-12 14:43:39 +0900

    🐛 fix:"현피 카드 수정 완료"

--- game.server/src/card/card.bbang.effect.ts
+++ game.server/src/card/card.bbang.effect.ts
@@ -1,8 +1,8 @@
 // cardType = 1
-import { getUserFromRoom, saveRoom, updateCharacterFromRoom } from "../utils/redis.util.js";
-import { CharacterStateType } from "../generated/common/enums.js";
-import { getRoom } from "../utils/redis.util.js";
-import { CardType } from "../generated/common/enums.js";
+import { getUserFromRoom, saveRoom, updateCharacterFromRoom } from '../utils/redis.util.js';
+import { CharacterStateType } from '../generated/common/enums.js';
+import { getRoom } from '../utils/redis.util.js';
+import { CardType } from '../generated/common/enums.js';
 
 const cardBbangEffect = async (roomId: number, userId: string, targetUserId: string) => {
 	// 정보값 가져오기
@@ -34,31 +34,33 @@ const cardBbangEffect = async (roomId: number, userId: string, targetUserId: str
 	}
 	if (!user || !target || !user.character || !target.character) return;
 
-	// 1. 방어 카드 확인 (C# 코드 참고)
-	// const defCardIndex = target.character.handCards.findIndex(c => c.rcode === "Shield");
-	// if (defCardIndex !== -1) {
-	//     const removedCard = target.character.handCards.splice(defCardIndex, 1)[0].rcode;
-	//     return { success: true, removedCard, hpChange: 0 };
-	// }
+	if (user.character.stateInfo.state === CharacterStateType.NONE_CHARACTER_STATE) {
+		// 상태 설정
+		user.character.stateInfo.state = CharacterStateType.BBANG_SHOOTER; // 빵야 카드 사용자는 BBANG_SHOOTER 상태가 되고
+		user.character.stateInfo.nextState = CharacterStateType.NONE_CHARACTER_STATE;
+		user.character.stateInfo.nextStateAt = `${Date.now() + 10000}`; //ms
+		user.character.stateInfo.stateTargetUserId = targetUserId; // 빵야 카드 사용자는 targetId에 대상자 ID를 기록
 
-	// 상태 설정
-	user.character.stateInfo.state = CharacterStateType.BBANG_SHOOTER; // 빵야 카드 사용자는 BBANG_SHOOTER 상태가 되고
-	user.character.stateInfo.nextState = CharacterStateType.NONE_CHARACTER_STATE;
-	user.character.stateInfo.nextStateAt = `${Date.now() + 10000}`; //ms
-	user.character.stateInfo.stateTargetUserId = targetUserId; // 빵야 카드 사용자는 targetId에 대상자 ID를 기록
+		target.character.stateInfo.state = CharacterStateType.BBANG_TARGET; // 빵야 카드 대상자는 BBANG_TARGET 상태가 됩니다
+		target.character.stateInfo.nextState = CharacterStateType.NONE_CHARACTER_STATE;
+		target.character.stateInfo.nextStateAt = `${Date.now() + 10000}`; //ms
+		target.character.stateInfo.stateTargetUserId = userId;
+	} else if(user.character.stateInfo.state === CharacterStateType.DEATH_MATCH_TURN_STATE){
+				// 상태 설정
+		user.character.stateInfo.state = CharacterStateType.DEATH_MATCH_STATE; // 빵야 카드 사용자는 BBANG_SHOOTER 상태가 되고
+		user.character.stateInfo.nextState = CharacterStateType.DEATH_MATCH_TURN_STATE;
+		user.character.stateInfo.nextStateAt = `${Date.now() + 10000}`; //ms
+		user.character.stateInfo.stateTargetUserId = targetUserId; // 빵야 카드 사용자는 targetId에 대상자 ID를 기록
 
-    target.character.stateInfo.state = CharacterStateType.BBANG_TARGET; // 빵야 카드 대상자는 BBANG_TARGET 상태가 됩니다
-    target.character.stateInfo.nextState = CharacterStateType.NONE_CHARACTER_STATE;
-    target.character.stateInfo.nextStateAt = `${Date.now()  + 10000}`; //ms
-    target.character.stateInfo.stateTargetUserId = userId;
+		target.character.stateInfo.state = CharacterStateType.DEATH_MATCH_TURN_STATE; // 빵야 카드 대상자는 BBANG_TARGET 상태가 됩니다
+		target.character.stateInfo.nextState = CharacterStateType.DEATH_MATCH_STATE;
+		target.character.stateInfo.nextStateAt = `${Date.now() + 10000}`; //ms
+		target.character.stateInfo.stateTargetUserId = userId;
+	}
 
-    console.log(`빵 카드 사용 됨`);
+	// 수정 정보 갱신
+	await updateCharacterFromRoom(roomId, userId, user.character);
+	await updateCharacterFromRoom(roomId, targetUserId, target.character);
+};
 
-    // 수정 정보 갱신
-    await updateCharacterFromRoom(roomId, userId, user.character)
-    await updateCharacterFromRoom(roomId, targetUserId, target.character);
-    
-}
-
-
-export default  cardBbangEffect;
+export default cardBbangEffect;
