6fa1a5d5cce620a8629581a6b720bb5b78864e0b
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-09-13 18:06:06 +0900

    ✅ test:"방생성 테스트 코드 작성

--- game.server/src/sockets/on.end.ts
+++ game.server/src/sockets/on.end.ts
@@ -4,17 +4,24 @@ import { removeSocket } from '../managers/socket.manger.js';
 import leaveRoomRequestHandler from '../handlers/request/leave.room.request.handler.js';
 import { deleteRoom, getRoom, removeUserFromRoom } from '../utils/redis.util.js';
 import { GameSocket } from '../type/game.socket.js';
+import { removeTokenUserDB } from '../services/prisma.service.js';
 
 const onEnd = (socket: GameSocket) => async () => {
 	try {
 		console.log('클라이언트 연결이 종료되었습니다.');
 		removeSocket(socket);
-		await removeUserFromRoom(Number(socket.roomId), socket.userId!);
-		const room = await getRoom(Number(socket.roomId));
-		if(room!.users.length <= 0){
-			await deleteRoom(room!.id);
-		}
 
+		if(socket.userId){
+			await removeTokenUserDB(Number(socket.userId));
+		}
+		
+		if (socket.roomId) {
+			await removeUserFromRoom(Number(socket.roomId), socket.userId!);
+			const room = await getRoom(Number(socket.roomId));
+			if (room!.users.length <= 0) {
+				await deleteRoom(room!.id);
+			}
+		}
 	} catch (error) {
 		handleError(socket, error);
 	}
