06a7d7dceb97d0eaf2e308ff4a8fbdcad1ab1de1
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-09-13 13:56:17 +0900

    ✅ test: "회원가입 테스트 코드 작성"

---
+++ game.server/src/services/register.request.handler/test/set.user.db.service.test.ts
@@ -0,0 +1,48 @@
+import setUserDbService from '../set.user.db.service';
+import { prisma } from '../../../utils/db';
+import * as bcrypt from 'bcrypt';
+import { C2SRegisterRequest } from '../../../generated/packet/auth';
+
+
+jest.mock('../../../utils/db', () => ({
+	prisma: {
+		user: {
+			create: jest.fn(),
+		},
+	},
+}));
+
+jest.mock('bcrypt', () => ({
+    hash: jest.fn(),
+}));
+
+
+describe('setUserDbService', () =>{
+    const mockReq ={
+        email: 'test@naver.com',
+        nickname:'qwer',
+        password: 'Password123!',
+    }
+    
+    afterEach(() =>{
+        jest.clearAllMocks();
+    });
+
+    it('비밀번호를 해시하고 DB에 유저 생성', async () =>{
+
+        (bcrypt.hash as jest.Mock).mockResolvedValue('hashedPassword');
+
+        await setUserDbService(mockReq);
+
+        expect(bcrypt.hash).toHaveBeenCalledWith(mockReq.password, 12);
+
+        expect(prisma.user.create).toHaveBeenCalledWith({
+            data:{
+                email: mockReq.email,
+                nickname: mockReq.nickname,
+                password: 'hashedPassword',
+            }
+        });
+    });
+
+} );
