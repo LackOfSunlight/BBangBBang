a37991a1e798e7a9fde07a94b53babeaa1b61516
gguti <ggutips94@gmail.com>
2025-09-18 14:31:07 +0900

    원본파일 주석처리

--- game.server/src/handlers/notification/game.prepare.notification.handler.ts
+++ game.server/src/handlers/notification/game.prepare.notification.handler.ts
@@ -1,35 +1,35 @@
-import { GameSocket } from '../../type/game.socket.js';
-import { S2CGamePrepareNotification } from '../../generated/packet/notifications.js';
-import { GamePacket } from '../../generated/gamePacket.js';
-import { GamePacketType } from '../../enums/gamePacketType.js';
-import { Room } from '../../models/room.model.js';
-import { sendData } from '../../utils/send.data.js';
-import { getRoom } from '../../utils/redis.util.js';
-import { broadcastDataToRoom } from '../../utils/notification.util.js';
+// import { GameSocket } from '../../type/game.socket.js';
+// import { S2CGamePrepareNotification } from '../../generated/packet/notifications.js';
+// import { GamePacket } from '../../generated/gamePacket.js';
+// import { GamePacketType } from '../../enums/gamePacketType.js';
+// import { Room } from '../../models/room.model.js';
+// import { sendData } from '../../utils/send.data.js';
+// import { getRoom } from '../../utils/redis.util.js';
+// import { broadcastDataToRoom } from '../../utils/notification.util.js';
 
-const gamePrepareNotificationHandler = async (socket: GameSocket, gamePacket: GamePacket) => {
-	const roomId = socket.roomId;
+// const gamePrepareNotificationHandler = async (socket: GameSocket, gamePacket: GamePacket) => {
+// 	const roomId = socket.roomId;
 
-	if (!roomId) return;
+// 	if (!roomId) return;
 
-	const room: Room | null = await getRoom(roomId);
+// 	const room: Room | null = await getRoom(roomId);
 
-	if (!room) return;
+// 	if (!room) return;
 
-	broadcastDataToRoom(room.users, gamePacket, GamePacketType.gamePrepareNotification);
-};
+// 	broadcastDataToRoom(room.users, gamePacket, GamePacketType.gamePrepareNotification);
+// };
 
-export const setGamePrepareNotification = (room: Room): GamePacket => {
-	const newGamePacket: GamePacket = {
-		payload: {
-			oneofKind: GamePacketType.gamePrepareNotification,
-			gamePrepareNotification: {
-				room,
-			},
-		},
-	};
+// export const setGamePrepareNotification = (room: Room): GamePacket => {
+// 	const newGamePacket: GamePacket = {
+// 		payload: {
+// 			oneofKind: GamePacketType.gamePrepareNotification,
+// 			gamePrepareNotification: {
+// 				room,
+// 			},
+// 		},
+// 	};
 
-	return newGamePacket;
-};
+// 	return newGamePacket;
+// };
 
-export default gamePrepareNotificationHandler;
+// export default gamePrepareNotificationHandler;
