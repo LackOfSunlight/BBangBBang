d7878c2bb56fa3d380231322f3bf747cfb44f6d9
gguti <ggutips94@gmail.com>
2025-09-19 13:10:47 +0900

    ♻️ refacto 오토쉴드 카드 장착 조건 제거/ 방어확률 함수 생성/testcode
    기존 동일한 장착카드를 장착하고 있어도 새로 업데이트 했을 때 클라이언트에서
    월드덱으로 반환하기 때문에 필요없는 조건문 삭제
    
    방어확률 함수로 처리해 reaction에서 받아서 처리할 수 있도록 수정
    
    변경된 코드에 맞춰 테스트 코드 수정

--- game.server/src/card/card.auto_shield.effect.ts
+++ game.server/src/card/card.auto_shield.effect.ts
@@ -1,24 +1,22 @@
 import { getUserFromRoom, updateCharacterFromRoom } from '../utils/room.utils';
 import { CardType } from '../generated/common/enums';
 
-export const equipAutoShieldEffect = async (roomId: number, userId: string): Promise<boolean> => {
-	const user = await getUserFromRoom(roomId, userId);
-
+export const equipAutoShieldEffect = (roomId: number, userId: string): boolean => {
+	const user = getUserFromRoom(roomId, userId);
 	// 유효성 검증
-	if (!user || !user.character) return false;
-
-	// 자동 쉴드 장착
-	if (!user.character.equips.includes(CardType.AUTO_SHIELD)) {
-		user.character.equips.push(CardType.AUTO_SHIELD);
-	} else {
+	if (!user || !user.character) {
 		return false;
 	}
 
-	// 정보 업데이트
+	// 자동 쉴드 장착
+	user.character.equips.push(CardType.AUTO_SHIELD);
+
+	// 정보 업데이트3
 	updateCharacterFromRoom(roomId, userId, user.character);
 	return true;
 };
 
-export const AutoShieldBlock = (): boolean => {
+// 피격 시 자동 쉴드의 25% 방어 확률을 계산하는 효과.
+export const autoShieldBlock = (): boolean => {
 	return Math.random() < 0.25;
 };
