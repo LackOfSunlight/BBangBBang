66344087f8f2d99fb901ab3736f2fa28caeed475
kimdandy <d22psource@gmail.com>
2025-09-20 17:22:05 +0900

    ✨ feat:'AUTO_RIFLE 카드 효과 처리 로직 에 사용시 카드를 제거하는 로직 추가'

--- game.server/src/card/card.auto_rifle.effect.ts
+++ game.server/src/card/card.auto_rifle.effect.ts
@@ -2,6 +2,7 @@
 
 import { CardType } from '../generated/common/enums';
 import { getUserFromRoom, updateCharacterFromRoom } from '../utils/room.utils';
+import { repeatDeck } from '../managers/card.manager';
 
 const cardAutoRifleEffect = (roomId: number, userId: string): boolean => {
 	// 정보값 가져오기
@@ -9,12 +10,22 @@ const cardAutoRifleEffect = (roomId: number, userId: string): boolean => {
 	// 유효성 검증
 	if (!user || !user.character || !user.character.stateInfo) return false;
 
-	// 소지 카드에서의 제거는 호출함수에서 처리
-	if (!user.character.equips.includes(CardType.AUTO_RIFLE)) {
-		user.character.weapon = CardType.AUTO_RIFLE; // 16;AUTO_RIFLE 장착
-	} else {
+	// 여러 검증을 했다면 카드 제거 및 검증 로직 실행
+	const haveCard = user.character.handCards.find(c => c.type === CardType.AUTO_RIFLE);
+	if (!haveCard) {
+		console.warn('[CardType:AUTO_RIFLE] 해당 카드를 소유하고 있지 않습니다')
 		return false;
 	}
+	haveCard.count -= 1;
+	if ( haveCard.count <=0 ){
+		const lastCardIndex = user.character.handCards.findIndex(c => c.type === CardType.AUTO_RIFLE);
+		user.character.handCards.splice(lastCardIndex, 1);
+	}
+	repeatDeck(roomId, [CardType.AUTO_RIFLE]);
+
+	
+	user.character.weapon = CardType.AUTO_RIFLE; // 16;AUTO_RIFLE 장착
+	
 
 	// 수정 정보 갱신
 	try {
