2c3f241628522f8fb101f30c704e9093b0eae1c5
jajeun <jaejun.pak@gmail.com>
2025-09-20 19:44:47 +0900

    ✨ feat: 팅기면 사망처리
    - 게임 중인 사용자가 연결이 끊어질 경우 HP를 0으로 설정하여 죽은 상태로 처리
    - 사용자 상태 업데이트 알림을 모든 플레이어에게 전송
    - 방에 사용자가 없을 경우 방 삭제 로직 유지

--- game.server/src/sockets/on.end.ts
+++ game.server/src/sockets/on.end.ts
@@ -2,7 +2,12 @@ import { handleError } from '../handlers/handleError.js';
 import { removeSocket } from '../managers/socket.manger.js';
 import { GameSocket } from '../type/game.socket.js';
 import { removeTokenUserDB } from '../services/prisma.service.js';
-import { deleteRoom, getRoom, removeUserFromRoom } from '../utils/room.utils.js';
+import { deleteRoom, getRoom, removeUserFromRoom, saveRoom } from '../utils/room.utils.js';
+import { checkAndEndGameIfNeeded } from '../utils/game.end.util.js';
+import { broadcastDataToRoom } from '../utils/notification.util.js';
+import { GamePacketType } from '../enums/gamePacketType.js';
+import { createUserUpdateNotificationPacket } from '../useCase/use.card/use.card.usecase.js';
+import { RoomStateType } from '../generated/common/enums.js';
 
 
 const onEnd = (socket: GameSocket) => async () => {
@@ -15,12 +20,35 @@ const onEnd = (socket: GameSocket) => async () => {
 		}
 
 		if (socket.roomId) {
-			
-			// await removeUserFromRoom();
-			removeUserFromRoom(Number(socket.roomId), socket.userId!);
 			const room = getRoom(Number(socket.roomId));
-			if (room!.users.length <= 0) {
-				deleteRoom(Number(socket.roomId));
+			if (room) {
+				// 게임 중인 경우 플레이어를 죽은 상태로 처리
+				if (room.state === RoomStateType.INGAME) {
+					const disconnectedUser = room.users.find(user => user.id === socket.userId);
+					if (disconnectedUser && disconnectedUser.character && disconnectedUser.character.hp > 0) {
+												
+						// 플레이어 HP를 0으로 설정하여 죽은 상태로 처리
+						disconnectedUser.character.hp = 0;
+						
+						// 방 상태 저장
+						saveRoom(room);
+						
+						// 모든 플레이어에게 사용자 상태 업데이트 알림 전송
+						const userUpdatePacket = createUserUpdateNotificationPacket(room.users);
+						await broadcastDataToRoom(room.users, userUpdatePacket, GamePacketType.userUpdateNotification);
+						
+						// 게임 종료 조건 확인
+						await checkAndEndGameIfNeeded(Number(socket.roomId));
+					}
+				}
+				
+				// 기존 로직: 방에서 사용자 제거
+				removeUserFromRoom(Number(socket.roomId), socket.userId!);
+				
+				// 방에 사용자가 없으면 방 삭제
+				if (room.users.length <= 0) {
+					deleteRoom(Number(socket.roomId));
+				}
 			}
 		}
 	} catch (error) {
