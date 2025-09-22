3dc419326b95b6fb1bbc046beb83794aaa7f2f85
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-09-22 20:01:38 +0900

    🐛 fix:"119 카드 오작동 오류 수정"

--- game.server/src/managers/game.manager.ts
+++ game.server/src/managers/game.manager.ts
@@ -37,7 +37,7 @@ class GameManager {
 		const phase = PhaseType.DAY;
 		roomPhase.set(roomId, phase);
 
-		const intervalId = setInterval(() => broadcastPositionUpdates(room), 200);
+		const intervalId = setInterval(() => broadcastPositionUpdates(room), 200000000);
 
 		positionUpdateIntervals.set(room.id, intervalId);
 		this.scheduleNextPhase(room.id, roomId);
