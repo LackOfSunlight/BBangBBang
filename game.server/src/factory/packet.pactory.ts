e12353fded7525033357349bd540330ffc7eb472
gguti <ggutips94@gmail.com>
2025-09-23 16:24:12 +0900

    ⚡improve: 팩토리 함수 주석 처리

--- game.server/src/factory/packet.pactory.ts
+++ game.server/src/factory/packet.pactory.ts
@@ -5,7 +5,7 @@ import {
 	CharacterPositionData,
 	GameStateData,
 	RoomData,
-    UserData,
+	UserData,
 } from '../generated/common/types';
 import { GamePacket } from '../generated/gamePacket';
 import { S2CGamePrepareResponse, S2CGameStartResponse } from '../generated/packet/game_actions';
@@ -18,30 +18,24 @@ import { S2CLeaveRoomResponse } from '../generated/packet/room_actions';
 import { Room } from '../models/room.model';
 import { User } from '../models/user.model';
 
-export const destroyResponseForm = (handCards: CardData[]): GamePacket => {
-	const newGamePacket: GamePacket = {
-		payload: {
-			oneofKind: GamePacketType.destroyCardResponse,
-			destroyCardResponse: {
-				handCards,
-			},
-		},
-	};
-
-	return newGamePacket;
-};
-
-export const createRoomResponseForm = (
+/**
+ * 회원가입 응답
+ * @param success
+ * @param message
+ * @param failCode
+ * @returns
+ */
+export const registerResponseForm = (
 	success: boolean,
+	message: string,
 	failCode: GlobalFailCode,
-	room?: RoomData,
 ): GamePacket => {
 	const newGamePacket: GamePacket = {
 		payload: {
-			oneofKind: GamePacketType.createRoomResponse,
-			createRoomResponse: {
+			oneofKind: GamePacketType.registerResponse,
+			registerResponse: {
 				success,
-				room,
+				message,
 				failCode,
 			},
 		},
@@ -50,168 +44,15 @@ export const createRoomResponseForm = (
 	return newGamePacket;
 };
 
-export const fleaMarketResponseForm = (success: boolean, failCode: GlobalFailCode): GamePacket => {
-	const newGamePacket: GamePacket = {
-		payload: {
-			oneofKind: GamePacketType.fleaMarketPickResponse,
-			fleaMarketPickResponse: {
-				success,
-				failCode,
-			},
-		},
-	};
-
-	return newGamePacket;
-};
-
-export const fleaMarketNotificationForm = (
-	cardTypes: CardType[],
-	pickIndex: number[],
-): GamePacket => {
-	const newGamePacket: GamePacket = {
-		payload: {
-			oneofKind: GamePacketType.fleaMarketNotification,
-			fleaMarketNotification: {
-				cardTypes,
-				pickIndex,
-			},
-		},
-	};
-
-	return newGamePacket;
-};
-
-export const gamePrepareResponsePacketForm = (payload: S2CGamePrepareResponse): GamePacket => {
-	return {
-		payload: {
-			oneofKind: GamePacketType.gamePrepareResponse,
-			gamePrepareResponse: payload,
-		},
-	};
-};
-
-export const gamePrepareNotificationPacketForm = (room: Room): GamePacket => {
-	const notificationPayload: S2CGamePrepareNotification = {
-		room: room,
-	};
-
-	return {
-		payload: {
-			oneofKind: GamePacketType.gamePrepareNotification,
-			gamePrepareNotification: notificationPayload,
-		},
-	};
-};
-
-export const gameStartResponsePacketForm = (payload: S2CGameStartResponse): GamePacket => {
-	return {
-		payload: {
-			oneofKind: GamePacketType.gameStartResponse,
-			gameStartResponse: payload,
-		},
-	};
-};
-
-export const gameStartNotificationPacketForm = (
-	gameState: GameStateData,
-	users: User[],
-	characterPositions: CharacterPositionData[],
-): GamePacket => {
-	const payload: S2CGameStartNotification = {
-		gameState,
-		users,
-		characterPositions,
-	};
-	return {
-		payload: {
-			oneofKind: GamePacketType.gameStartNotification,
-			gameStartNotification: payload,
-		},
-	};
-};
-
-export const getRoomListResponseForm = (rooms: Room[]): GamePacket => {
-	const newGamaPacket: GamePacket = {
-		payload: {
-			oneofKind: GamePacketType.getRoomListResponse,
-			getRoomListResponse: {
-				rooms,
-			},
-		},
-	};
-
-	return newGamaPacket;
-};
-
-export const joinRandomRoomResponseForm = (
-	success: boolean,
-	failCode: GlobalFailCode,
-	room?: Room,
-): GamePacket => {
-	const newGamePacket: GamePacket = {
-		payload: {
-			oneofKind: GamePacketType.joinRandomRoomResponse,
-			joinRandomRoomResponse: {
-				success,
-				room,
-				failCode,
-			},
-		},
-	};
-
-	return newGamePacket;
-};
-
-export const joinRoomNotificationForm = (joinUser: User): GamePacket => {
-	const newGamePacket: GamePacket = {
-		payload: {
-			oneofKind: GamePacketType.joinRoomNotification,
-			joinRoomNotification: {
-				joinUser,
-			},
-		},
-	};
-
-	return newGamePacket;
-};
-
-export const joinRoomResponseForm = (
-	success: boolean,
-	failCode: GlobalFailCode,
-	room?: Room,
-): GamePacket => {
-	const newGamePacket: GamePacket = {
-		payload: {
-			oneofKind: GamePacketType.joinRoomResponse,
-			joinRoomResponse: {
-				success,
-				room,
-				failCode,
-			},
-		},
-	};
-
-	return newGamePacket;
-};
-
-export const leaveRoomResponsePacketForm = (payload: S2CLeaveRoomResponse): GamePacket => {
-	return {
-		payload: {
-			oneofKind: GamePacketType.leaveRoomResponse,
-			leaveRoomResponse: payload,
-		},
-	};
-};
-
-export const userLeftNotificationPacketForm = (payload: S2CLeaveRoomNotification): GamePacket => {
-	return {
-		payload: {
-			oneofKind: GamePacketType.leaveRoomNotification,
-			leaveRoomNotification: payload,
-		},
-	};
-};
-
+/**
+ * 로그인 응답
+ * @param success
+ * @param message
+ * @param token
+ * @param failCode
+ * @param myInfo
+ * @returns
+ */
 export const loginResponseForm = (
 	success: boolean,
 	message: string,
@@ -235,6 +76,285 @@ export const loginResponseForm = (
 	return newGamePacket;
 };
 
+/**
+ *  방 생성 응답
+ * @param success
+ * @param failCode
+ * @param room
+ * @returns
+ */
+export const createRoomResponseForm = (
+	success: boolean,
+	failCode: GlobalFailCode,
+	room?: RoomData,
+): GamePacket => {
+	const newGamePacket: GamePacket = {
+		payload: {
+			oneofKind: GamePacketType.createRoomResponse,
+			createRoomResponse: {
+				success,
+				room,
+				failCode,
+			},
+		},
+	};
+
+	return newGamePacket;
+};
+
+/**
+ * 게임준비 응답
+ * @param payload
+ * @returns
+ */
+export const gamePrepareResponsePacketForm = (payload: S2CGamePrepareResponse): GamePacket => {
+	return {
+		payload: {
+			oneofKind: GamePacketType.gamePrepareResponse,
+			gamePrepareResponse: payload,
+		},
+	};
+};
+/**
+ * 게임 준비 알림
+ * @param room
+ * @returns
+ */
+export const gamePrepareNotificationPacketForm = (room: Room): GamePacket => {
+	const notificationPayload: S2CGamePrepareNotification = {
+		room: room,
+	};
+
+	return {
+		payload: {
+			oneofKind: GamePacketType.gamePrepareNotification,
+			gamePrepareNotification: notificationPayload,
+		},
+	};
+};
+
+/**
+ * 게임시작 응답
+ * @param payload
+ * @returns
+ */
+export const gameStartResponsePacketForm = (payload: S2CGameStartResponse): GamePacket => {
+	return {
+		payload: {
+			oneofKind: GamePacketType.gameStartResponse,
+			gameStartResponse: payload,
+		},
+	};
+};
+
+/**
+ * 게임시작 알림
+ * @param gameState
+ * @param users
+ * @param characterPositions
+ * @returns
+ */
+export const gameStartNotificationPacketForm = (
+	gameState: GameStateData,
+	users: User[],
+	characterPositions: CharacterPositionData[],
+): GamePacket => {
+	const payload: S2CGameStartNotification = {
+		gameState,
+		users,
+		characterPositions,
+	};
+	return {
+		payload: {
+			oneofKind: GamePacketType.gameStartNotification,
+			gameStartNotification: payload,
+		},
+	};
+};
+
+/**
+ * 방 목록 응답
+ * @param rooms
+ * @returns
+ */
+export const getRoomListResponseForm = (rooms: Room[]): GamePacket => {
+	const newGamaPacket: GamePacket = {
+		payload: {
+			oneofKind: GamePacketType.getRoomListResponse,
+			getRoomListResponse: {
+				rooms,
+			},
+		},
+	};
+
+	return newGamaPacket;
+};
+
+/**
+ * 랜덤 방 참여 응답
+ * @param success
+ * @param failCode
+ * @param room
+ * @returns
+ */
+export const joinRandomRoomResponseForm = (
+	success: boolean,
+	failCode: GlobalFailCode,
+	room?: Room,
+): GamePacket => {
+	const newGamePacket: GamePacket = {
+		payload: {
+			oneofKind: GamePacketType.joinRandomRoomResponse,
+			joinRandomRoomResponse: {
+				success,
+				room,
+				failCode,
+			},
+		},
+	};
+
+	return newGamePacket;
+};
+
+/**
+ * 방 참여 알림
+ * @param joinUser
+ * @returns
+ */
+export const joinRoomNotificationForm = (joinUser: User): GamePacket => {
+	const newGamePacket: GamePacket = {
+		payload: {
+			oneofKind: GamePacketType.joinRoomNotification,
+			joinRoomNotification: {
+				joinUser,
+			},
+		},
+	};
+
+	return newGamePacket;
+};
+
+/**
+ * 방 참여 응답
+ * @param success
+ * @param failCode
+ * @param room
+ * @returns
+ */
+export const joinRoomResponseForm = (
+	success: boolean,
+	failCode: GlobalFailCode,
+	room?: Room,
+): GamePacket => {
+	const newGamePacket: GamePacket = {
+		payload: {
+			oneofKind: GamePacketType.joinRoomResponse,
+			joinRoomResponse: {
+				success,
+				room,
+				failCode,
+			},
+		},
+	};
+
+	return newGamePacket;
+};
+
+/**
+ * 방 나가기 응답
+ * @param payload
+ * @returns
+ */
+export const leaveRoomResponsePacketForm = (payload: S2CLeaveRoomResponse): GamePacket => {
+	return {
+		payload: {
+			oneofKind: GamePacketType.leaveRoomResponse,
+			leaveRoomResponse: payload,
+		},
+	};
+};
+
+/**
+ * 방 나간유저 알림
+ * @param payload
+ * @returns
+ */
+export const userLeftNotificationPacketForm = (payload: S2CLeaveRoomNotification): GamePacket => {
+	return {
+		payload: {
+			oneofKind: GamePacketType.leaveRoomNotification,
+			leaveRoomNotification: payload,
+		},
+	};
+};
+
+/**
+ * 밤시간 카드 제거 응답
+ * @param handCards
+ * @returns
+ */
+export const destroyResponseForm = (handCards: CardData[]): GamePacket => {
+	const newGamePacket: GamePacket = {
+		payload: {
+			oneofKind: GamePacketType.destroyCardResponse,
+			destroyCardResponse: {
+				handCards,
+			},
+		},
+	};
+
+	return newGamePacket;
+};
+
+/**
+ * 플리마켓 응답
+ * @param success
+ * @param failCode
+ * @returns
+ */
+export const fleaMarketResponseForm = (success: boolean, failCode: GlobalFailCode): GamePacket => {
+	const newGamePacket: GamePacket = {
+		payload: {
+			oneofKind: GamePacketType.fleaMarketPickResponse,
+			fleaMarketPickResponse: {
+				success,
+				failCode,
+			},
+		},
+	};
+
+	return newGamePacket;
+};
+
+/**
+ * 플리마켓 알림
+ * @param cardTypes
+ * @param pickIndex
+ * @returns
+ */
+export const fleaMarketNotificationForm = (
+	cardTypes: CardType[],
+	pickIndex: number[],
+): GamePacket => {
+	const newGamePacket: GamePacket = {
+		payload: {
+			oneofKind: GamePacketType.fleaMarketNotification,
+			fleaMarketNotification: {
+				cardTypes,
+				pickIndex,
+			},
+		},
+	};
+
+	return newGamePacket;
+};
+
+/**
+ * 디버프 패스 응답
+ * @param success
+ * @param failCode
+ * @returns
+ */
 export const passDebuffResponseForm = (success: boolean, failCode: GlobalFailCode): GamePacket => {
 	const newGamePacket: GamePacket = {
 		payload: {
@@ -249,25 +369,13 @@ export const passDebuffResponseForm = (success: boolean, failCode: GlobalFailCod
 	return newGamePacket;
 };
 
-export const registerResponseForm = (
-	success: boolean,
-	message: string,
-	failCode: GlobalFailCode,
-): GamePacket => {
-	const newGamePacket: GamePacket = {
-		payload: {
-			oneofKind: GamePacketType.registerResponse,
-			registerResponse: {
-				success,
-				message,
-				failCode,
-			},
-		},
-	};
-
-	return newGamePacket;
-};
-
+/**
+ * 카드사용 알림
+ * @param cardType
+ * @param userId
+ * @param targetUserId
+ * @returns
+ */
 export const useCardNotificationPacketForm = (
 	cardType: CardType,
 	userId: string,
@@ -287,6 +395,11 @@ export const useCardNotificationPacketForm = (
 	return NotificationPacket;
 };
 
+/**
+ * 유저정보 업데이트 알림
+ * @param user
+ * @returns
+ */
 export const userUpdateNotificationPacketForm = (user: User[]): GamePacket => {
 	const NotificationPacket: GamePacket = {
 		payload: {
@@ -300,8 +413,11 @@ export const userUpdateNotificationPacketForm = (user: User[]): GamePacket => {
 	return NotificationPacket;
 };
 
-
-
+/**
+ * 포지션 업데이트 알림
+ * @param characterPositions
+ * @returns
+ */
 export const positionUpdateNotificationForm = (
 	characterPositions: CharacterPositionData[],
 ): GamePacket => {
@@ -316,6 +432,13 @@ export const positionUpdateNotificationForm = (
 	return newGamePacket;
 };
 
+/**
+ * 페이즈업데이트 알림
+ * @param phaseType
+ * @param nextPhaseAt
+ * @param characterPositions
+ * @returns
+ */
 export const phaseUpdateNotificationForm = (
 	phaseType: PhaseType, // DAY 1, EVENING 2, END 3
 	nextPhaseAt: string, // 다음 페이즈 시작 시점(밀리초 타임스탬프)
@@ -335,6 +458,12 @@ export const phaseUpdateNotificationForm = (
 	return newGamePacket;
 };
 
+/**
+ * 게임 종료 알림
+ * @param winners
+ * @param winType
+ * @returns
+ */
 export const gameEndNotificationForm = (winners: string[], winType: WinType): GamePacket => {
 	const newGamePacket: GamePacket = {
 		payload: {
