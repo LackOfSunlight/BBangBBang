0bf0f8b6a36fba898109b7200b742bf1ed30269a
gguti <ggutips94@gmail.com>
2025-09-26 17:51:01 +0900

    ♻️ refactor : removeCard 개별 로직에 추가

--- game.server/src/card/active/card.vaccine.effect.ts
+++ game.server/src/card/active/card.vaccine.effect.ts
@@ -1,11 +1,16 @@
 import { User } from '../../models/user.model';
 import { Room } from '../../models/room.model';
 import { applyHeal } from '../CardService/applyHeal';
+import { cardManager } from '../../managers/card.manager';
+import { CardType, SelectCardType } from '../../generated/common/enums';
 
 const cardVaccineEffect = (room: Room, user: User): boolean => {
 	if (!user || !room) return false;
 
 	const HEAL_AMOUNT = 1;
+
+	cardManager.removeCard(user, room, CardType.VACCINE);
+	
 	return applyHeal(user, HEAL_AMOUNT);
 };
 
