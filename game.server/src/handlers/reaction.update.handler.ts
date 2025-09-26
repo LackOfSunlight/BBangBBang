1efb94bd2ccba1285d15fad97af618ea23b250d2
kimdandy <d22psource@gmail.com>
2025-09-26 13:47:14 +0900

    📝 chore:'PascalCase 적용'

--- game.server/src/handlers/reaction.update.handler.ts
+++ game.server/src/handlers/reaction.update.handler.ts
@@ -14,19 +14,19 @@ const reactionUpdateHandler = async (socket: GameSocket, gamePacket: GamePacket)
 	const { userId, roomId } = socket;
 	if (!userId || !roomId) {
 		// DTO가 유효하지 않으면 즉시 에러 응답
-		is_invalid_request(socket, GlobalFailCode.INVALID_REQUEST);
+		InvalidRequest(socket, GlobalFailCode.INVALID_REQUEST);
 		return;
 	}
 
 	const room: Room | null = roomManger.getRoom(roomId);
 	if (!room) {
-		is_invalid_request(socket, GlobalFailCode.ROOM_NOT_FOUND);
+		InvalidRequest(socket, GlobalFailCode.ROOM_NOT_FOUND);
 		return;
 	}
 
 	const payload = getGamePacketType(gamePacket, gamePackTypeSelect.reactionRequest);
 	if (!payload) {
-		is_invalid_request(socket, GlobalFailCode.INVALID_REQUEST);
+		InvalidRequest(socket, GlobalFailCode.INVALID_REQUEST);
 		return;
 	}
 
@@ -45,7 +45,7 @@ const reactionUpdateHandler = async (socket: GameSocket, gamePacket: GamePacket)
 };
 
 /** 오류코드:잘못된요청 을 일괄 처리하기 위한 함수 */
-const is_invalid_request = (socket: GameSocket, failcode: GlobalFailCode) => {
+const InvalidRequest = (socket: GameSocket, failcode: GlobalFailCode) => {
 	const wrongDTO = reactionResponsePacketForm(false, failcode);
 	sendData(socket, wrongDTO, GamePacketType.useCardResponse);
 };
