83e6333851ae6012449f76d89be77ffc86926dc7
gguti <ggutips94@gmail.com>
2025-09-26 17:47:56 +0900

    Update apply.card.use.handler.ts

--- game.server/src/handlers/apply.card.use.handler.ts
+++ game.server/src/handlers/apply.card.use.handler.ts
@@ -1,4 +1,4 @@
-import roomManger from '../managers/room.manager';
+import roomManager from '../managers/room.manager';
 import { Room } from '../models/room.model';
 import { User } from '../models/user.model';
 
@@ -7,16 +7,18 @@ export const applyCardUseHandler = (
 	userId: string,
 	targetId: string,
 ): { room: Room; user: User; target: User } => {
-	const room = roomManger.getRoom(roomId);
-	const user = roomManger.getUserFromRoom(roomId, userId);
-	if (targetId != '0') {
-		const target = roomManger.getUserFromRoom(roomId, targetId);
-		return { room, user, target };
+	let target: User;
+
+	const room = roomManager.getRoom(roomId);
+	const user = roomManager.getUserFromRoom(roomId, userId);
+	if (targetId) {
+		target = roomManager.getUserFromRoom(roomId, targetId);
 	} else {
-		const target = {
+		target = {
 			id: '0',
 			nickname: 'none',
 		};
-		return { room, user, target };
 	}
+
+	return { room, user, target };
 };
